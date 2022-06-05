a2442e9332486c8914bc03a7a0a67d9b7120482a
EbenauDev <ebenau06@gmail.com>
2022-06-05 20:12:04 -0300

    API - Ajustado método OnModelCreating para ignorar as entidades, ValidationResult e Event

--- src/services/NSE.Cliente.API/Data/ClientesContext.cs
+++ src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -27,6 +27,10 @@ namespace NSE.Clientes.API.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            modelBuilder.Ignore<ValidationResult>();
+            modelBuilder.Ignore<Event>();
+
             foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
                 e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
                 property.SetColumnType("varchar(100)");
