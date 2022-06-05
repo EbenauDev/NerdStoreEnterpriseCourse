905625f401538ccf01a3f33ef0ba0eead6965831
EbenauDev <ebenau06@gmail.com>
2022-06-05 20:07:32 -0300

    API - Criado controller ClientesController

---
+++ src/services/NSE.Cliente.API/Controllers/ClientesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebAPI.Core.Controllers;
+using System;
+using System.Threading.Tasks;
+
+namespace NSE.Clientes.API.Controllers
+{
+    public class ClientesController : MainController
+    {
+
+        [HttpGet("clientes")]
+        public async Task<IActionResult> Index()
+        {
+            return Ok();
+        }
+    }
+}
